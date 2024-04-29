024727039cde4fbbee8875522233b6739efdcb39
Sahil <sahilrangari07@gmail.com>
2024-04-29 15:33:32 +0530

    first commit

---
+++ src/main/java/com/JapaneseTeacher/JapaneseTeacherApplication.java
@@ -0,0 +1,13 @@
+package com.JapaneseTeacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapaneseTeacherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapaneseTeacherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JapaneseTeacher/JapaneseTeacherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JapaneseTeacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapaneseTeacherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
